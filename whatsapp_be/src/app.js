29d652c68c9b27642c876135ddedb7e0e6a207a6
Ashutosh Choudhary <ashutoshchoudhary998@gmail.com>
2023-07-18 21:43:58 +0530

    middleware setup

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -1,10 +1,56 @@
-import express from "express"
+import express, { json } from "express"
+import dotenv from 'dotenv'
+import morgan from "morgan"
+import helmet from "helmet"
+import mongoSanitize from 'express-mongo-sanitize'
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import fileUpload from "express-fileupload"
+import cors from 'cors'
 
+//dotenv config
+dotenv.config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('hello')
+//Morgan
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan("dev"))
+}
+
+//Helmet
+app.use(helmet())
+
+//json
+app.use(express.json())
+
+//parse json requesr body
+app.use(express.urlencoded({ extended: true }))
+
+//sanitize request data
+app.use(mongoSanitize())
+
+//parse cookie
+app.use(cookieParser())
+
+//gzip compression
+app.use(compression())
+
+//file upload
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+//cors
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+
+
+
+app.post('/test', (req, res) => {
+    res.send(req.body)
 })
 
 
--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,10 +1,14 @@
-import dotenv from 'dotenv'
+
 import app from './app.js'
 
-//dotenv config
-dotenv.config()
 
-const port = dotenv.PORT || 8000
+
+
+//env variables
+const port = process.env.PORT || 7000
+
+console.log(process.env.NODE_ENV);
+
 app.listen(port, () => {
     console.log(`listining on port ${port}`);
 })
